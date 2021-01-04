3d5ffc8e71b5401ee7558a1c3af120a2952a0ae7
Jordan Weiner <weinerj8@gmail.com>
2021-01-04 15:33:17 -0800

    Added a footer to mimic the look of a 'real' website. Only the styling, no actual functionality as of now.

---
+++ client/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import HoverText from './HoverText.jsx';
+
+const FooterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px solid #294059;
+  font-size: 12px;
+  margin-top: 20px;
+  padding: 10px 0;
+`;
+
+const FooterOptionsContainer = styled.span`
+  display: flex;
+  justify-content: space-between;
+  width: 18%;
+`;
+
+const FooterOption = ({ text }) => (
+  <HoverText
+    effect="background"
+    text={text}
+    regColor="#f5f5f5"
+    hoveredColor="rgba(41, 64, 89, .2)"
+  />
+);
+
+const Footer = () => (
+  <FooterContainer>
+    <FooterOptionsContainer>
+      <FooterOption text="About"/>|
+      <FooterOption text="Contact"/>|
+      <FooterOption text="Terms of Use"/>|
+      <FooterOption text="Privacy"/>
+    </FooterOptionsContainer>
+    <span><i className="far fa-copyright"></i> 2021 Jordan Weiner</span>
+  </FooterContainer>
+);
+
+export default Footer;
--- client/index.js
+++ client/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import Footer from './components/Footer.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+ReactDOM.render(<Footer />, document.getElementById('footer'));
