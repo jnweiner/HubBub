f87c34e813057cf7a68e7ada920e2d0b9212213f
Jordan Weiner <weinerj8@gmail.com>
2021-03-14 14:59:33 -0700

    Write tests for HoverText

--- client/tests/App.test.js
+++ client/tests/App.test.js
@@ -5,6 +5,7 @@ import App from '../components/App.jsx';
 describe('<App />', () => {
   const wrapper = shallow(<App />, { disableLifecycleMethods: true });
   it('displays LoggedOutContainer when currentUser is set to null', () => {
-    expect(wrapper.find('LoggedOutContainer')).toBeTruthy();
+    expect(wrapper.find('LoggedOutContainer')).toHaveLength(1);
+    expect(wrapper.find('LoggedInContainer')).toHaveLength(0);
   });
 });
